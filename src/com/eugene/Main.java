adaf05e6a3e6902f6617dc03fbfa0392ac33d40d
EUGENE OCHIENG <ochiengeugene008@gmail.com>
2022-03-20 11:32:54 +0300

    version2

--- src/com/eugene/Main.java
+++ src/com/eugene/Main.java
@@ -1,8 +1,22 @@
 package com.eugene;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        int age;
+        String name,Color;
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Name: ");
+        name=scanner.next();
+        System.out.print("Age: ");
+        age=scanner.nextInt();
+        System.out.print("Color: ");
+        Color=scanner.next();
+
+
+        System.out.println(name+" of age "+ age+" like color "+Color);
     }
 }
